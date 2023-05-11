ad9fd7fc4294647ff246c95f1613492dbfc9cb84
yx-xiong <xyxuana@mails.ccnu.edu.cn>
2023-05-11 21:32:54 +0800

    Update

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mtANN", 
+<<<<<<< HEAD
+    version="0.1.0",
+    py_modules=['mtANN.model', 'mtANN.utils'],
+=======
     version="0.0.0",
     py_modules=['mtANN.mtANN_model', 'mtANN.mtANN_utils'],
+>>>>>>> 72453049c5c54617b0d1fca69290eae901d86269
     author="Yi-Xuan Xiong",
     author_email="xyxuana@mails.ccnu.edu.cn",
     description="Ensemble Multiple References for Single-cell RNA Seuquencing Data Annotation and Unseen Cells Identification",
